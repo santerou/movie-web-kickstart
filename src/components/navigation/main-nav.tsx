811bc35d5683fa17d944e5edae2dc403ef4e2dff
santerou <santerou@gmail.com>
2025-11-23 19:26:25 +0100

    Update main-nav.tsx

--- src/components/navigation/main-nav.tsx
+++ src/components/navigation/main-nav.tsx
@@ -220,13 +220,7 @@ export function MainNav({ items }: MainNavProps) {
           onChangeStatusOpen={handleChangeStatusOpen}
           containerClassName={cn(path === '/' ? 'hidden' : 'flex')}
         />
-        <Link
-          rel="noreferrer"
-          target="_blank"
-          href={siteConfig.links.github}
-          className={cn(path === '/' ? 'flex' : 'hidden')}>
-          <Icons.gitHub className="h-5 w-5 hover:bg-transparent" />
-        </Link>
+        
         <ThemeToggle />
       </div>
     </nav>
