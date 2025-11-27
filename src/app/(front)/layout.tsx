66e1d60508c9e40c02d8b3c990a62508703563bf
santerou <santerou@gmail.com>
2025-11-27 21:06:42 +0100

    Update layout.tsx

--- src/app/(front)/layout.tsx
+++ src/app/(front)/layout.tsx
@@ -1,88 +1,15 @@
-import { TailwindIndicator } from '@/components/tailwind-indicator';
-import { ThemeProvider } from '@/components/theme-provider';
-import { cn } from '@/lib/utils';
-import '@/styles/globals.css';
-import type { Metadata, Viewport } from 'next';
-import { Inter as FontSans } from 'next/font/google';
-import localFont from 'next/font/local';
-import { Analytics } from '@/components/analytics';
-import { siteConfig } from '@/configs/site';
-import { env } from '@/env.mjs';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import Script from 'next/script';
+import SiteFooter from '@/components/main/site-footer';
+import SiteHeader from '@/components/main/site-header';
+
+const FrontLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen">
+      <SiteHeader />
+      <main>{children}</main>
+      <SiteFooter />
+    </div>
+  );
+};
 
 export const runtime = 'edge';
-
-const fontSans = FontSans({ subsets: ['latin'], variable: '--font-sans', display: 'swap' });
-
-const fontHeading = localFont({ src: '../assets/fonts/CalSans-SemiBold.woff2', variable: '--font-heading' });
-
-export const viewport: Viewport = {
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
-};
-
-export const metadata: Metadata = {
-  metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
-  title: {
-    default: siteConfig.name, // site name only
-    template: `%s - ${siteConfig.name}`, // pages prepend their own title
-  },
-  description: siteConfig.description,
-  keywords: siteConfig.keywords,
-  authors: [{ name: siteConfig.author, url: siteConfig.url }],
-  openGraph: {
-    type: 'website',
-    locale: 'en_US',
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: siteConfig.ogImage,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: siteConfig.author,
-  },
-  icons: { icon: '/favicon.ico' },
-  other: { referrer: 'no-referrer-when-downgrade' },
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={cn('overflow-y-auto min-h-screen overflow-x-hidden bg-background font-sans antialiased', fontSans.variable, fontHeading.variable)}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
-          <TailwindIndicator />
-          <Analytics />
-          <SpeedInsights />
-          {env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && (
-            <>
-              <Script
-                id="_next-ga-init"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){window.dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', { cookie_flags: 'max-age=86400;secure;samesite=none' });
-                  `,
-                }}
-              />
-              <Script
-                id="_next-ga"
-                src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-              />
-            </>
-          )}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+export default FrontLayout;
