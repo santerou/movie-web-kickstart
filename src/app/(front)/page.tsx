05364987b19ef0abf57e634b679e1183b7dcee13
santerou <santerou@gmail.com>
2025-11-30 17:18:32 +0100

    Update page.tsx

--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -1,19 +1,11 @@
-import { siteConfig } from "@/configs/site";
 import { Icons } from "@/components/icons";
 import { Badge } from "@/components/ui/badge";
 import { buttonVariants } from "@/components/ui/button";
+import { siteConfig } from "@/configs/site";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: siteConfig.slogan, // layout prepends site name
-  description: siteConfig.description,
-  keywords: siteConfig.keywords,
-};
 
 export const runtime = 'edge';
-
 export default function Index() {
   return (
     <>
@@ -23,20 +15,21 @@ export default function Index() {
         className="container mx-auto flex flex-col items-center justify-center gap-4 pb-8 pt-28 text-center md:pb-12 lg:py-32"
       >
         <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
-          <Badge aria-hidden="true" className="rounded-md px-3.5 py-1.5" variant="secondary">
+          <Badge
+            aria-hidden="true"
+            className="rounded-md px-3.5 py-1.5"
+            variant="secondary"
+          >
             <Icons.twitter className="mr-2 h-3.5 w-3.5" />
             Follow along on Twitter
           </Badge>
           <span className="sr-only">Twitter</span>
         </Link>
-
-        <h1
-          id="hero-heading"
-          className="max-w-screen-lg text-center font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl"
-        >
-          {siteConfig.slogan} {/* no site name here */}
+        <h1 className="max-w-screen-lg text-center font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+          {siteConfig.name} - {siteConfig.slogan}
+          {/* {siteConfig.name} - watch tv shows online, watch movies online. */}
+          {/* An e-commerce skateshop built with everything new in Next.js 13 */}
         </h1>
-
         <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           Step into a world where entertainment knows no boundaries, where your
           screens come alive with an endless array of captivating stories.
